ad2345f03d6de0b5297334f3c7aba13feca190e9
pixelready <bizartist@gmail.com>
2022-01-12 15:16:47 -0800

    generate HTML board

--- connect-four/connect4.js
+++ connect-four/connect4.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /** Connect Four
  *
@@ -18,135 +18,132 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-
-  let board = new Array(HEIGHT).fill(Array(WIDTH).fill(null));
-  console.log(board);
-
+	board = new Array(HEIGHT).fill(Array(WIDTH).fill(null));
+	console.log(board);
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+	// get "htmlBoard" variable from the item in HTML w/ID of "board"
+	const htmlBoard = document.getElementById('board');
 
-  // TODO: add comment for this code
-  const top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
+	// generate tr for top row of game board, set id and add click handler for player moves
+	const top = document.createElement('tr');
+	top.setAttribute('id', 'column-top');
+	top.addEventListener('click', handleClick);
 
-  // TODO: add comment for this code
-  for (let x = 0; x < WIDTH; x++) {
-    let headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
-    top.append(headCell);
-  }
-  htmlBoard.append(top);
+	// add head cells to top row
+	for (let x = 0; x < WIDTH; x++) {
+		let headCell = document.createElement('td');
+		headCell.setAttribute('id', x);
+		top.append(headCell);
+	}
+	htmlBoard.append(top);
 
-  // dynamically creates the main part of html board
-  // uses HEIGHT to create table rows
-  // uses WIDTH to create table cells for each row
-  for (let y = 0; y < HEIGHT; y++) {
-    // TODO: Create a table row element and assign to a "row" letiable
+	// dynamically creates the main part of html board
+	// uses HEIGHT to create table rows
+	// uses WIDTH to create table cells for each row
+	for (let y = 0; y < HEIGHT; y++) {
+		// Create a table row element and assign to a "row" variable
 
-    for (let x = 0; x < WIDTH; x++) {
-      // TODO: Create a table cell element and assign to a "cell" variable
+		const row = document.createElement('tr');
 
-      // TODO: add an id, y-x, to the above table cell element
-      // you'll use this later, so make sure you use y-x
-
-      // TODO: append the table cell to the table row
-
-    }
-    // TODO: append the row to the html board
-
-  }
+		for (let x = 0; x < WIDTH; x++) {
+			// TODO: Create a table cell element and assign to a "cell" variable
+			let cell = document.createElement('td');
+			// TODO: add an id, y-x, to the above table cell element
+			cell.setAttribute('id', `${y}-${x}`);
+			// you'll use this later, so make sure you use y-x
+			// TODO: append the table cell to the table row
+			row.append(cell);
+		}
+		// TODO: append the row to the html board
+		htmlBoard.append(row);
+	}
 }
 
 /** findSpotForCol: given column x, return bottom empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 5
-  return 5;
+	// TODO: write the real version of this, rather than always returning 5
+	return 5;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+	// TODO: make a div and insert into correct table cell
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+	// TODO: pop up alert message
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
-  // get x from ID of clicked cell
-  let x = +evt.target.id;
+	// get x from ID of clicked cell
+	let x = +evt.target.id;
 
-  // get next spot in column (if none, ignore click)
-  let y = findSpotForCol(x);
-  if (y === null) {
-    return;
-  }
+	// get next spot in column (if none, ignore click)
+	let y = findSpotForCol(x);
+	if (y === null) {
+		return;
+	}
 
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  placeInTable(y, x);
+	// place piece in board and add to HTML table
+	// TODO: add line to update in-memory board
+	placeInTable(y, x);
 
-  // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
+	// check for win
+	if (checkForWin()) {
+		return endGame(`Player ${currPlayer} won!`);
+	}
 
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+	// check for tie
+	// TODO: check if all cells in board are filled; if so call, call endGame
 
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
+	// switch players
+	// TODO: switch currPlayer 1 <-> 2
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
 function checkForWin() {
-
-  /** _win:
+	/** _win:
    * takes input array of 4 cell coordinates [ [y, x], [y, x], [y, x], [y, x] ]
    * returns true if all are legal coordinates for a cell & all cells match
    * currPlayer
    */
-  function _win(cells) {
+	function _win(cells) {
+		// TODO: Check four cells to see if they're all legal & all color of current
+		// player
+	}
 
-    // TODO: Check four cells to see if they're all legal & all color of current
-    // player
+	// using HEIGHT and WIDTH, generate "check list" of coordinates
+	// for 4 cells (starting here) for each of the different
+	// ways to win: horizontal, vertical, diagonalDR, diagonalDL
+	for (let y = 0; y < HEIGHT; y++) {
+		for (let x = 0; x < WIDTH; x++) {
+			// TODO: assign values to the below variables for each of the ways to win
+			// horizontal has been assigned for you
+			// each should be an array of 4 cell coordinates:
+			// [ [y, x], [y, x], [y, x], [y, x] ]
 
-  }
+			let horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
+			let vert;
+			let diagDL;
+			let diagDR;
 
-  // using HEIGHT and WIDTH, generate "check list" of coordinates
-  // for 4 cells (starting here) for each of the different
-  // ways to win: horizontal, vertical, diagonalDR, diagonalDL
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      // TODO: assign values to the below variables for each of the ways to win
-      // horizontal has been assigned for you
-      // each should be an array of 4 cell coordinates:
-      // [ [y, x], [y, x], [y, x], [y, x] ]
-
-      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert;
-      let diagDL;
-      let diagDR;
-
-      // find winner (only checking each win-possibility as needed)
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-        return true;
-      }
-    }
-  }
+			// find winner (only checking each win-possibility as needed)
+			if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+				return true;
+			}
+		}
+	}
 }
 
 makeBoard();
